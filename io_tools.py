515c3451790f5fbae574ab8b800d5122eac7ae1f
Varun Nagaraja <varun@umiacs.umd.edu>
2014-10-30 12:01:00 -0400

    Initial files.
    	new file:   io_tools.py
    	new file:   track_operations.py

---
+++ io_tools.py
@@ -0,0 +1,99 @@
+import os
+import ipdb
+
+import cv
+import cv2
+
+from dataset import *
+
+colors = [cv.CV_RGB(255,0,0), cv.CV_RGB(0,0,255), cv.CV_RGB(255,255,0), 
+          cv.CV_RGB(128,128,128)]
+
+
+def cap_number(num,low,high):
+  '''
+  Returns a number that is within [low,high]. 
+  If num < low, low is returned.
+  if num > high, high is returned.  
+  '''
+  if num < low:
+    return low
+  elif num > high:
+    return high
+  else:
+    return num
+  
+
+def annotate_frames(input_video_file, output_video_file, tracks):
+  '''
+  Draws bounding boxes of the tracks along with some text if it exists.
+  '''
+  max_frame_over_all = 0
+  for i in tracks:
+    t = tracks[i].track
+    max_frame = max(t.keys())
+    if max_frame > max_frame_over_all:
+      max_frame_over_all = max_frame
+
+  track_ids_per_frame = [[]] * int(max_frame_over_all+1)
+  for i in tracks:
+    t = tracks[i]
+    for frame in t.track:
+      if len(track_ids_per_frame[int(frame)]) == 0:
+        track_ids_per_frame[int(frame)] = [t.obj_id]
+      else:
+        track_ids_per_frame[int(frame)].append(t.obj_id)
+      
+  video_input = cv2.VideoCapture(input_video_file)
+  fps = video_input.get(cv.CV_CAP_PROP_FPS)
+  fourcc_code = int(video_input.get(cv.CV_CAP_PROP_FOURCC))
+  wd = int(video_input.get(cv.CV_CAP_PROP_FRAME_WIDTH))
+  ht = int(video_input.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
+  fourcc = cv.CV_FOURCC('X','V','I','D')
+  
+  video_output = cv2.VideoWriter(output_video_file,fourcc,fps,(wd,ht))
+  
+  frame_num = 0
+  ret, frame_img = video_input.read()
+  while ret:
+    frame_num += 1
+    #frame_img[:] = 0
+    if frame_num < len(track_ids_per_frame):
+      tracks_in_curr_frame = track_ids_per_frame[frame_num]
+      for track in tracks_in_curr_frame:
+        bbox = tracks[track].track[frame_num]
+        bbox_x1 = int(cap_number(bbox[0],0,wd))
+        bbox_y1 = int(cap_number(bbox[1],0,ht))
+        if tracks[track].track_format == 'wd_ht':
+          bbox_x2 = int(cap_number(bbox[0]+bbox[2],0,wd))
+          bbox_y2 = int(cap_number(bbox[1]+bbox[3],0,ht))
+        elif tracks[track].track_format == 'two_points':
+          bbox_x2 = int(cap_number(bbox[2],0,wd))
+          bbox_y2 = int(cap_number(bbox[3],0,ht))
+        obj_id = tracks[track].obj_id
+        
+        box_color = colors[int(obj_id) % len(colors)] 
+        cv2.rectangle(frame_img,(bbox_x1,bbox_y1),(bbox_x2,bbox_y2),box_color,10)
+        if frame_num in tracks[track].attributes:
+          attribute_text = tracks[track].attributes[frame_num]
+          if len(attribute_text) > 0: 
+            cv2.putText(frame_img,attribute_text,(bbox_x1, bbox_y1-5),
+                        cv2.FONT_HERSHEY_SIMPLEX,1,box_color,3)
+    
+    video_output.write(frame_img)
+    ret, frame_img = video_input.read()
+    
+    
+  video_input.release()
+  video_output.release()  
+
+
+def split_video(in_filename, start_time, end_time, out_filename):
+  '''
+  Runs a system command to trim the video. Uses ffmpeg.
+  '''
+  os.system('ffmpeg -i '+in_filename+' -ss '+str(start_time)+
+            ' -to '+str(end_time)+ ' -async 1 ' + out_filename)
+  
+
+     
---
+++ track_operations.py
@@ -0,0 +1,134 @@
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+
+class Track:
+  def __init__(self, obj_id, obj_type, track_format):
+    '''
+    Parameters
+    ----------
+    obj_id:  unique id for an object track
+    obj_type: category for the track - person, car etc.
+    track_format: 'wd_ht' or 'two_points'
+    
+    track is a dictionary - {frame: [bbox_x, bbox_y, bbox_wd, bbox_ht], ...}
+    attribute is also a dictionary - {frame: 'text', ...}
+    '''
+    self.obj_id = obj_id
+    self.obj_type = obj_type
+    self.track = {}
+    self.attributes = {}
+    if track_format is not 'wd_ht' and track_format is not 'two_points':
+      raise ValueError()
+    else: 
+      self.track_format = track_format 
+      
+    
+  def append_to_track(self,frame,bbox):
+    '''
+    Appends frame by frame to an existing track.
+    
+    track = {frame: [bbox_x, bbox_y, bbox_wd, bbox_ht], ...}
+    '''
+    self.track[int(frame)] = bbox
+    
+    
+  def append_to_attributes(self,attribute):
+    '''
+    Appends an entire attribute to the existing one.
+    
+    Attribute is represented as text for each frame.
+    '''
+    for frame in attribute:
+      if frame in self.attributes:
+        self.attributes[frame] += "," + attribute[frame]
+      else:
+        self.attributes[frame] = attribute[frame]
+        
+  def get_tracklets(self):
+    '''
+    Returns list of tracklets in the track.
+    A track can have breaks and the smaller tracks are called tracklets.
+    [(s1,e1),(s2,e2),(s3,e3),...]
+    Here start and end frames are inclusive of the track endings.
+    '''
+    frame_nums = np.hstack((-1, np.array(sorted(self.track.keys()))))
+    frame_nums = np.hstack((frame_nums, frame_nums[-1]+2))
+    frame_shift = np.array(frame_nums[1:]) - np.array(frame_nums[0:-1])
+    frame_jumps = np.where(frame_shift>1)[0]
+    tracklets = [(frame_nums[frame_jumps[i]+1], frame_nums[frame_jumps[i+1]]) 
+                 for i in range(len(frame_jumps)-1)]
+    return tracklets
+
+  
+def get_tracklets(trackish):
+  '''
+  Returns list of tracklets in the tracklike object i.e. a dictionary with
+  frames as keys.
+  [(s1,e1),(s2,e2),(s3,e3),...]
+  Here start and end frames are inclusive of the track endings.
+  '''
+  frame_nums = np.hstack((-1, np.array(sorted(trackish.keys()))))
+  frame_nums = np.hstack((frame_nums, frame_nums[-1]+2))
+  frame_shift = np.array(frame_nums[1:]) - np.array(frame_nums[0:-1])
+  frame_jumps = np.where(frame_shift>1)[0]
+  tracklets = [(frame_nums[frame_jumps[i]+1], frame_nums[frame_jumps[i+1]]) 
+               for i in range(len(frame_jumps)-1)]
+  return tracklets
+
+
+def clip_track(track, start_frame, end_frame):
+  '''
+  Returns a new track object with a track between start_frame and end_frame.
+  The frame number starts from 1 and ends at (end_frame - start_frame + 1)
+  '''
+  clipped_track = Track(track.obj_id, track.obj_type, track.track_format)
+  for frame in track.track:
+    if frame >= start_frame and frame <= end_frame :
+      clipped_track.append_to_track(frame-start_frame+1, track.track[frame])
+  
+  return clipped_track
+
+
+def smoothen_track(t):
+  '''
+  Linear interpolation for missing detections.
+  Performs smoothing for the tracklets after interpolation.
+  Updates the input track.
+  '''
+  
+  #Interpolation to fill in the gaps of length at most 10 frames
+  frame_nums = sorted(t.track.keys())
+  for i in range(len(frame_nums)-1):
+    curr_frame = frame_nums[i]
+    next_frame = frame_nums[i+1]
+    diff_frames = next_frame - curr_frame
+    if diff_frames < 2 or diff_frames > 10:
+      continue
+    init_bbox = np.array(t.track[curr_frame])
+    final_bbox = np.array(t.track[next_frame])
+    diff_bbox = np.divide(np.subtract(final_bbox, init_bbox),float(diff_frames))
+    for j in range(1,diff_frames):
+      t.track[curr_frame + j] = (init_bbox + j * diff_bbox).tolist() 
+    
+  # Gaussian smoothing
+  # The smoothing occurs only over tracklets with no breaks. Any breaks left over
+  # after interpolation will not be touched.
+  tracklets = t.get_tracklets()
+
+  for (tracklet_start_frame, tracklet_end_frame) in tracklets:
+    bboxes = [t.track[frame] for frame in range(tracklet_start_frame,
+                                              tracklet_end_frame+1)]
+    smooth_bboxes = _smoothen_tracklet(bboxes).tolist()
+    for frame in range(tracklet_start_frame,tracklet_end_frame+1):
+      t.track[frame] = smooth_bboxes[frame-tracklet_start_frame]
+    
+  
+def _smoothen_tracklet(bboxes):
+  '''
+  Applies gaussian filter along the columns of the 2D array bboxes.
+  '''
+  smooth_bboxes = np.array(bboxes)
+  for i in range(smooth_bboxes.shape[1]):
+    smooth_bboxes[:,i] = gaussian_filter(smooth_bboxes[:,i],10)
+    
+  return smooth_bboxes
